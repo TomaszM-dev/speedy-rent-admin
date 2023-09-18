6728b1e61a8d521b117f91aee620b0435cdf6802
TomaszM-dev <tomekmalocha@gmail.com>
2023-09-18 16:01:11 +0200

    regrouping files, creating store

--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -1,36 +1,26 @@
 "use client";
 
 import * as z from "zod";
-import axios from "axios";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
-import { toast } from "react-hot-toast";
-import { useRouter } from "next/navigation";
-import { useState } from "react";
-
-import { Modal } from "@/components/ui/modal";
-import { Input } from "@/components/ui/input";
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
 import { useStoreModal } from "@/hooks/use-store-modal";
-import { Button } from "@/components/ui/button";
+import React, { useState } from "react";
+
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Form, FormControl, FormField, FormItem, FormLabel } from "../ui/form";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+import axios from "axios";
+import { toast } from "react-hot-toast";
+import { redirect } from "next/navigation";
+import { Modal } from "../ui/modal";
 
 const formSchema = z.object({
-  name: z.string().min(1),
+  name: z.string().min(2),
 });
 
-export const StoreModal = () => {
+const StoreModal = () => {
+  const [loading, setIsLoading] = useState(false);
   const storeModal = useStoreModal();
-  const router = useRouter();
-
-  const [loading, setLoading] = useState(false);
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
@@ -40,68 +30,67 @@ export const StoreModal = () => {
   });
 
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
-    console.log(values);
-    // try {
-    //   setLoading(true);
-    //   const response = await axios.post("/api/stores", values);
-    //   window.location.assign(`/${response.data.id}`);
-    // } catch (error) {
-    //   toast.error("Something went wrong");
-    // } finally {
-    //   setLoading(false);
-    // }
+    try {
+      setIsLoading(true);
+
+      const res = await axios.post("/api/stores", values);
+
+      toast.success("Store created.");
+      setTimeout(() => {
+        window.location.assign(`/${res.data.id}`);
+      }, 400);
+    } catch (error) {
+      toast.error("Something went wrong");
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   return (
     <Modal
-      title="Create store"
-      description="Add a new store to manage products and categories."
+      title="Create Store"
+      description="Add a new store to menage products"
       isOpen={storeModal.isOpen}
       onClose={storeModal.onClose}
     >
       <div>
-        <div className="space-y-4 py-2 pb-4">
-          <div className="space-y-2">
-            <Form {...form}>
-              <form onSubmit={form.handleSubmit(onSubmit)}>
-                <FormField
-                  control={form.control}
-                  name="name"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Name</FormLabel>
-                      <FormControl>
-                        <Input
-                          disabled={loading}
-                          placeholder="E-Commerce"
-                          {...field}
-                        />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <div className="pt-6 space-x-2 flex items-center justify-end w-full">
-                  <Button
-                    disabled={loading}
-                    variant="outline"
-                    onClick={storeModal.onClose}
-                  >
-                    Cancel
-                  </Button>
-                  <Button
-                    disabled={loading}
-                    type="submit"
-                    variant="destructive"
-                  >
-                    Continue
-                  </Button>
-                </div>
-              </form>
-            </Form>
-          </div>
+        <div className="py-2 pb-4 space-y-4">
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Name</FormLabel>
+                    <FormControl>
+                      <Input
+                        disabled={loading}
+                        placeholder="E-commerse"
+                        {...field}
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <div className="pt-6 space-x-2 flex items-center justify-end w-full">
+                <Button
+                  disabled={loading}
+                  variant="outline"
+                  onClick={storeModal.onClose}
+                >
+                  Cancel
+                </Button>
+                <Button disabled={loading} type="submit">
+                  Continue
+                </Button>
+              </div>
+            </form>
+          </Form>
         </div>
       </div>
     </Modal>
   );
 };
+
+export default StoreModal;
